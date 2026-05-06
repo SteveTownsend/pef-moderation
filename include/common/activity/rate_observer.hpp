49ca97405101d80f39829175544c6fcc51481c06
Steve Townsend <SteveTownsend0@gmail.com>
2026-05-06 11:46:56 -0400

    Proper rate limiting on label operations

--- include/common/activity/rate_observer.hpp
+++ include/common/activity/rate_observer.hpp
@@ -21,24 +21,60 @@ http://www.fsf.org/licensing/licenses
 *************************************************************************/
 
 #include <chrono>
+#include <cmath>
+#include <mutex>
+
+#include "common/log_wrapper.hpp"
 
 namespace activity {
 
 using std::chrono::system_clock;
 // after
 // https://www.rdiachenko.com/posts/arch/rate-limiting/sliding-window-algorithm/
-template <typename TIME_UNIT, typename COUNT_UNIT> class rate_observer {
-public:
+template <typename TIME_UNIT, typename COUNT_UNIT>
+class rate_observer {
+ public:
   rate_observer() = delete;
   rate_observer(TIME_UNIT const window_size, COUNT_UNIT const limit)
       : _window_size(
             std::chrono::duration_cast<system_clock::duration>(window_size)),
         _limit(limit) {
     _current_fixed_end = system_clock::now() + _window_size;
+    _event_interval = std::chrono::duration_cast<system_clock::duration>(
+        std::max(system_clock::duration(TIME_UNIT(window_size.count() / limit)),
+                 system_clock::duration(1)));
+    REL_INFO(
+        "Initialized rate observer with window size {} and limit {}, "
+        "calculated event interval {}",
+        _window_size.count(), _limit, _event_interval.count());
   }
   ~rate_observer() = default;
-  // Calculates excess over allowed observations
+
+  // Calculates excess over allowed observations, assuiming this observation is
+  // added.
   COUNT_UNIT observe_and_get_excess() {
+    std::lock_guard lock(_lock);
+    COUNT_UNIT requests(requests_so_far() + 1);
+    ++_current_count;  // record this observation
+    return std::max(COUNT_UNIT(0), requests - _limit);
+  }
+
+  // Determines if current request is allowed and opens gate if so
+  bool observe_if_permitted() {
+    std::lock_guard lock(_lock);
+    COUNT_UNIT requests(requests_so_far());
+    if (requests >= _limit) {
+      return false;
+    } else {
+      ++_current_count;  // record this observation
+      return true;
+    }
+  }
+
+  system_clock::duration event_interval() const { return _event_interval; }
+
+ private:
+  COUNT_UNIT requests_so_far() {
     system_clock::time_point now = system_clock::now();
     if (now > _current_fixed_end) {
       _last_fixed_end = _current_fixed_end;
@@ -51,25 +87,25 @@ public:
         std::max(0.0f, static_cast<float>(
                            (_last_fixed_end - sliding_window_start).count())) /
         static_cast<float>(_window_size.count());
-    COUNT_UNIT requests(static_cast<COUNT_UNIT>(
+    return static_cast<COUNT_UNIT>(
         std::floor((previous_window_weight * static_cast<float>(_last_count)) +
-                   static_cast<float>(++_current_count))));
-    return std::max(COUNT_UNIT(0), requests - _limit);
+                   static_cast<float>(++_current_count)));
   }
 
-private:
   // storage optimized order of fields
   system_clock::time_point _last_fixed_end = {};
   system_clock::time_point _current_fixed_end;
   system_clock::duration _window_size;
+  system_clock::duration _event_interval;
   COUNT_UNIT _last_count = 0;
   COUNT_UNIT _current_count = 0;
   COUNT_UNIT _limit;
+  std::mutex _lock;
   static_assert(std::is_integral<COUNT_UNIT>::value,
                 "COUNT_UNIT must be an integral type");
   static_assert(std::is_signed<COUNT_UNIT>::value,
                 "COUNT_UNIT must be a signed type");
 };
 
-} // namespace activity
+}  // namespace activity
 #endif
